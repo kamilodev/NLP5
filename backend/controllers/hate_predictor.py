3e72364f0a8218655510348c1d525f3871a9ec4d
ProKav <kamilo_vanegas@hotmail.com>
2023-11-14 22:53:12 +0100

    refactor: Implementing logic in make_prediction function for scrapping YouTube video, also, this function has control exceptions

--- backend/controllers/hate_predictor.py
+++ backend/controllers/hate_predictor.py
@@ -1,30 +1,44 @@
 from fastapi import Response, status
+from pydantic import BaseModel
+
+from . import scrapper
 
 
-async def receive_message(response:Response):
+class Text(BaseModel):
+    topic: str
+
+
+async def receive_message(response: Response):
     """
     Recibimos a través del endpoint el mensaje desde streamlit
     """
     try:
-        return {
-            "message": "Todo es correcto"
-            }
+        return {"message": "Todo es correcto"}
         response.status_code = status.HTTP_200_OK
     except Exception as error:
-        return{
-            "message": f"No se pudo realizar la solicitud, {error}"
-        }
+        return {"message": f"No se pudo realizar la solicitud, {error}"}
 
-async def make_prediction(text: str, response:Response):
+
+async def make_prediction(text, response: Response):
+    """
+    Realizamos la predicción
+    """
+    df = scrapper.scrapper_video(text.get("topic"), response)
+    if response.status_code == 200:
+        response.status_code = status.HTTP_200_OK
+        return df.to_dict(orient="records")
+    elif response.status_code == 404:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"El video {text.get('topic')} no existe"}
+
+
+async def make_mood_prediction(text, response: Response):
     """
     Realizamos la predicción
     """
     try:
-        print(text)
-        return True
+        return {"message": f"El texto es: {text.get('mood')}"}
         response.status_code = status.HTTP_200_OK
 
     except Exception as error:
-        return{
-            "message": f"Hubo un problema, {error}"
-        }
+        return {"message": f"Hubo un problema, {error}"}
