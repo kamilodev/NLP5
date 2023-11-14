a2112b6ab79c642df5a6cc9513dc46e52e2e107e
ProKav <kamilo_vanegas@hotmail.com>
2023-11-14 23:03:05 +0100

    refactor: Change logic in prediction function. There are two provisional action buttons, one for send URL, and another for send a simple text

--- frontend/pages/03_🤖_Predicción.py
+++ frontend/pages/03_🤖_Predicción.py
@@ -1,20 +1,15 @@
 import streamlit as st
 from assets.styles.css import styles
-from views.messages import AUTHORS
-from controllers.send_info import send_info
+from controllers.send_info import send_text, send_text_area
+
 
 def prediction():
-    st.set_page_config(
-        page_title='Predicción Hate',
-        page_icon='🤖'
-    )
-
+    st.set_page_config(page_title="Predicción Hate", page_icon="🤖")
     st.markdown(styles, unsafe_allow_html=True)
+
     st.title("🤖 Predicción Hate")
-    
-    st.markdown(
-    "Escribe a continuación el ID del comentario que deseas predecir"
-)
+
+    st.markdown("Escribe a continuación el ID del comentario que deseas predecir")
 
     topic = st.text_input(label="Comentario de YouTube", placeholder="ID")
     mood = st.text_area(
@@ -24,14 +19,12 @@ def prediction():
 
     col1, col2 = st.columns(2)
     with col1:
-        st.session_state.feeling_lucky = not st.button(
-            label="Predecir",
-            type="primary",
-            on_click=send_info,
-            args=(topic, mood),
-        )
-    
-    st.markdown(AUTHORS, unsafe_allow_html=True)
-    
+        if st.button(label="Predecir (Topic)", type="primary"):
+            send_text(topic)
+    with col2:
+        if st.button(label="Predecir (Mood)", type="primary"):
+            send_text_area(mood)
+
+
 if __name__ == "__main__":
     prediction()
