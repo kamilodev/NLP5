1289d461bd7381ef5c1fd20eadc8604e1a406910
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2023-11-15 01:33:18 +0100

    fix: Change logic from two buttons to only one, control input errors for empty or invalid fields, validate YouTube URL

--- frontend/pages/03_🤖_Predicción.py
+++ frontend/pages/03_🤖_Predicción.py
@@ -4,7 +4,8 @@ from controllers.send_info import send_text, send_text_area
 
 
 def prediction():
-    st.set_page_config(page_title="Predicción Hate", page_icon="🤖")
+    base_url = "https://www.youtube.com/watch?v="
+    st.set_page_config(page_title="Predicción Hate", page_icon="🤖", layout="wide")
     st.markdown(styles, unsafe_allow_html=True)
 
     st.title("🤖 Predicción Hate")
@@ -17,13 +18,16 @@ def prediction():
         placeholder="Escribe aquí",
     )
 
-    col1, col2 = st.columns(2)
-    with col1:
-        if st.button(label="Predecir (Topic)", type="primary"):
-            send_text(topic)
-    with col2:
-        if st.button(label="Predecir (Mood)", type="primary"):
+    if st.button(label="Predecir", type="primary"):
+        if topic:
+            if base_url in topic:
+                send_text(topic)
+            else:
+                st.error("Introduce una URL válida de YouTube")
+        if mood:
             send_text_area(mood)
+        if not topic and not mood:
+            st.error("Debes escribir algo para poder predecir")
 
 
 if __name__ == "__main__":
