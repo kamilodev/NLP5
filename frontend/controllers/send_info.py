bfb50e22b25b487b6d023123af0a6640afd5b9e1
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2023-11-15 01:30:43 +0100

    refactor: Change the way of render messages from st.success to st.write, add red color to mood text

--- frontend/controllers/send_info.py
+++ frontend/controllers/send_info.py
@@ -12,20 +12,17 @@ def send_text(topic):
             )
             if response.status_code == 200:
                 result = response.json()
-                print("results: ", result)
                 dataset = pd.DataFrame(result)
-                st.success(
-                    "¡Hemos hecho scrapping a tu video! 🥳 Observa los resultados:"
-                )
-                st.dataframe(dataset)
+
+                st.write("")
+                st.success("¡Hemos hecho scrapping a tu video! 🥳")
+                st.write("Numero de comentarios: ", len(dataset))
+                st.write("")
+                st.dataframe(dataset, width=1600, height=900)
             else:
                 st.error(response.json().get("message"))
         except Exception as e:
             st.error(f"Ocurrió un error: {e}")
-    else:
-        st.warning(
-            "Por favor, ingresa el ID del comentario para hacer la predicción del tema."
-        )
 
 
 def send_text_area(mood):
@@ -36,13 +33,12 @@ def send_text_area(mood):
             response = requests.post("http://localhost:8000/predict/mood", json=payload)
             if response.status_code == 200:
                 result = response.json().get("message")
+                mood = payload["mood"]
+
+                st.write("")
                 print("results: ", result)
-                st.success(f"Predicción del texto {payload['mood']}: {result}")
+                st.write(f":red[_La predicción del texto_ {mood}: {result}]")
             else:
                 st.error("Error en la solicitud. Inténtalo de nuevo más tarde.")
         except Exception as e:
             st.error(f"Ocurrió un error: {e}")
-    else:
-        st.warning(
-            "Por favor, ingresa el texto para hacer la predicción del estado de ánimo."
-        )
