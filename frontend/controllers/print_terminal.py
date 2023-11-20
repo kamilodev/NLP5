ec9124a3375ad7855558233e5106682a01ed925b
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2023-11-20 16:56:11 +0100

    feat: Create a function that provide the same results of trained model, but, this info is rendered in terminal CLI

---
+++ frontend/controllers/print_terminal.py
@@ -0,0 +1,57 @@
+import argparse
+import requests
+import pandas as pd
+import sys
+
+def get_dataset_from_url(url):
+    payload = {"topic": url}
+    try:
+        response = requests.post("http://127.0.0.1:8000/predict/message?text", json=payload)
+        if response.status_code == 200:
+            result = response.json()
+            return pd.DataFrame(result)
+        else:
+            print(f"Error: {response.json().get('message')}")
+            return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def get_prediction_from_text(text):
+    payload = {"mood": text}
+    try:
+        response = requests.post("http://127.0.0.1:8000/predict/mood", json=payload)
+        if response.status_code == 200:
+            result = response.json()
+            mood = payload["mood"]
+            print(result["message"])
+            print("\n")
+        else:
+            print("Error en la solicitud. Inténtalo de nuevo más tarde.")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Get dataset from URL or text prediction")
+    parser.add_argument("input", help="URL or text to send for prediction")
+    parser.add_argument("--mode", choices=["url", "text"], default="url", help="Mode of operation (url or text)")
+
+    args = parser.parse_args()
+    input_data = args.input
+    mode = args.mode
+
+    print("\033c")
+    print("\nEstamos trabajando en las predicciones, espera un momento...\n")
+
+    if mode == "text":
+        get_prediction_from_text(input_data)
+    elif mode == "url":
+        dataset = get_dataset_from_url(input_data)
+        if dataset is not None:
+            pd.set_option("display.max_rows", None)
+            print(dataset)
+    else:
+        print("Modo no válido. Use 'url' o 'text'.")
+
+if __name__ == "__main__":
+    main()
